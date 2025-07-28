485d5379791f1f7ce55ecc7800e03e50912dda85
Josh Lankford <lankford.josh@gmail.com>
2025-07-28 09:14:28 -0500

    setup resource operation type

--- src/resource.zig
+++ src/resource.zig
@@ -15,10 +15,42 @@ pub fn Resource(comptime T: type) type {
     };
 }
 
+pub fn OperationResult(comptime T: type) type {
+    return struct {
+        const Self = @This();
+
+        resource: ?T = null,
+        status_code: u16,
+        success: bool,
+
+        pub fn init() Self {
+            return Self{
+                .status_code = 0,
+                .success = false,
+            };
+        }
+
+        pub fn isSuccess(self: Self) bool {
+            return self.success and self.status_code >= 200 and self.status_code < 300;
+        }
+    };
+}
+
 test "basic resource test" {
     const Patient = @import("model/patient.zig");
     const PatientResource = Resource(Patient.Patient);
 
     const resource_name = PatientResource.getResourceType();
     try std.testing.expect(std.mem.eql(u8, resource_name, "Patient"));
+}
+
+test "basic OperationResult test" {
+    const Patient = @import("model/patient.zig");
+    const PatientResource = Resource(Patient.Patient);
+
+    var result = OperationResult(PatientResource).init();
+    result.success = true;
+    result.status_code = 200;
+
+    try std.testing.expect(result.isSuccess());
 }
